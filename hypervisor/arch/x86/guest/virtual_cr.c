zhangqiang4/acrn-hypervisor

commit 9cca11f0c3908e33d0bf19b2bc5a9ca5687793ff

hv: fix cr0 write check on clearing CR0.PG when in long mode
According to SDM Vol.2 "MOV—Move to/from Control Registers", #GP(0)
should be generated in this case.

Tracked-On: ACRN-10839
Signed-off-by: Qiang Zhang <qiang4.zhang@intel.com>

hypervisor/arch/x86/guest/virtual_cr.c
@@ -188,13 +188,27 @@ static bool is_cr0_write_valid(struct acrn_vcpu *vcpu, uint64_t cr0)
 				if (((cr0 & CR0_CD) == 0UL) && ((cr0 & CR0_NW) != 0UL)) {
 					ret = false;
 				}
-				/* SDM 4.10.1 "Process-Context Identifiers"
-				 *
-				 * MOV to CR0 causes a general-protection exception if it would
-				 * clear CR0.PG to 0 while CR4.PCIDE = 1
-				 */
-				if (((cr0 & CR0_PG) == 0UL) && ((vcpu_get_cr4(vcpu) & CR4_PCIDE) != 0UL)) {
-					ret = false;
+
+				if ((cr0 & CR0_PG) == 0UL) {
+					if ((vcpu_get_efer(vcpu) & MSR_IA32_EFER_LME_BIT) != 0UL) {
+						/* SDM Vol.2 "MOV—Move to/from Control Registers"
+						 *
+						 * In 64-Bit mode, clearing CR0.PG causes #GP(0).
+						 */
+						ret = false;
+					} else if ((vcpu_get_cr4(vcpu) & CR4_PCIDE) != 0UL) {
+						/* SDM 4.10.1 "Process-Context Identifiers"
+						 *
+						 * MOV to CR0 causes a general-protection exception if it would
+						 * clear CR0.PG to 0 while CR4.PCIDE = 1 and in
+						 * compatibility mode.
+						 */
+						ret = false;
+					} else {
+						/* CR0.PG is allowed to be 0 in compatibility/protected
+						 * mode with CR4.PCIDE = 0, and real-address mode.
+						 */
+					}
 				}
 			}
 		}

commit 1882d99d48959ba6f4acef79e00820e6c4ae3655

hv: fix 64-bit cpu mode check in cr0 virtualization
MSR_IA32_EFER_LME_BIT can't tell the vCPU is in 64-bit long mode. We
should further check CS.L = 1. vcpu->arch.cpu_mode saves current vCPU
mode and it's updated after every VM-exit.

Fixes: 9cca11f0c ("hv: fix cr0 write check on clearing CR0.PG when in long mode")
Tracked-On: ACRN-10839
Signed-off-by: Qiang Zhang <qiang4.zhang@intel.com>

hypervisor/arch/x86/guest/virtual_cr.c
@@ -190,7 +190,7 @@ static bool is_cr0_write_valid(struct acrn_vcpu *vcpu, uint64_t cr0)
 				}
 
 				if ((cr0 & CR0_PG) == 0UL) {
-					if ((vcpu_get_efer(vcpu) & MSR_IA32_EFER_LME_BIT) != 0UL) {
+					if (vcpu->arch.cpu_mode == CPU_MODE_64BIT) {
 						/* SDM Vol.2 "MOV—Move to/from Control Registers"
 						 *
 						 * In 64-Bit mode, clearing CR0.PG causes #GP(0).